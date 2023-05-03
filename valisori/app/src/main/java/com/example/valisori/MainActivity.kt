bc46d4260d1792fe871c3f26f65e2685b8ddba75
Sori <sorinapopaan@gmail.com>
2023-05-04 00:22:49 +0300

    Changed project to MVVM

--- valisori/app/src/main/java/com/example/valisori/MainActivity.kt
+++ valisori/app/src/main/java/com/example/valisori/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.valisori
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.valisori.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val mainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.lifecycleOwner = this
+        binding.mainViewModel = mainViewModel
+        setContentView(binding.root)
+
     }
 }
---
+++ valisori/app/src/main/java/com/example/valisori/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.valisori
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+
+class MainViewModel(application:Application) : AndroidViewModel(application) {
+    val inputText = MutableLiveData("")
+    val onInputButtonClicked = MutableLiveData(false)
+
+    fun onClickInputButton() {
+        onInputButtonClicked.value = true
+    }
+}
