694bb59a6311bb6d817df1553a3c00e6771abe03
Sori <sorinapopaan@gmail.com>
2023-05-13 20:23:51 +0300

    Added Vocal User Input

--- valisori/app/src/main/java/com/example/valisori/MainActivity.kt
+++ valisori/app/src/main/java/com/example/valisori/MainActivity.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.content.getSystemService
 import com.example.valisori.databinding.ActivityMainBinding
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.database.FirebaseDatabase
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val mainViewModel: MainViewModel by viewModels()
     private lateinit var userInput: TextInputLayout
+    private val SPEECH_RECOGNIZER_REQUEST_CODE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,21 +27,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         userInput = binding.userInputText
 
-
-
-
-//        sharedPreferences = getSharedPreferences(
-//            getString(R.string.shared_preference_organiser), Context.MODE_PRIVATE
-//        )
-//        subscribeToObserver()
         sendDataToFirebaseObserver()
+        recordDataFromUserObserver()
     }
 
     private fun sendDataToFirebaseObserver() {
         //initialise Firebase database and reference
         val database = FirebaseDatabase.getInstance()
         val timestamp = System.currentTimeMillis().toString()
-        val myRef = database.getReference("data").child(timestamp)
+        val myRef = database.getReference("data")
 
         mainViewModel.onInputButtonClicked.observe(this) { value ->
             if (value) {
@@ -47,11 +44,45 @@ class MainActivity : AppCompatActivity() {
                     val timestamp = System.currentTimeMillis().toString()
                     val childRef = myRef.child(timestamp)
                     childRef.setValue(data)
-                    Toast.makeText(this, "Data pushed to server with timestamp $timestamp", Toast.LENGTH_LONG).show()
+                    Toast.makeText(
+                        this,
+                        "Data pushed to server with timestamp $timestamp",
+                        Toast.LENGTH_LONG
+                    ).show()
                 } else {
                     Toast.makeText(this, "Please enter data", Toast.LENGTH_LONG).show()
                 }
             }
         }
     }
+
+    private fun recordDataFromUserObserver() {
+        mainViewModel.onVoiceButtonClicked.observe(this) { value ->
+            if (value) {
+                val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+                speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+                speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak now!")
+                startActivityForResult(speechRecognizerIntent, SPEECH_RECOGNIZER_REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == SPEECH_RECOGNIZER_REQUEST_CODE && resultCode == RESULT_OK) {
+            val matches: ArrayList<String>? = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            if (matches != null && matches.isNotEmpty()) {
+                val spokenText = matches[0]
+                userInput.editText?.setText(spokenText)
+            }
+        }
+    }
+
+
+
+
+
+
+
 }
--- valisori/app/src/main/java/com/example/valisori/MainViewModel.kt
+++ valisori/app/src/main/java/com/example/valisori/MainViewModel.kt
@@ -4,12 +4,15 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 
-class MainViewModel(application:Application) : AndroidViewModel(application) {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
     val inputText = MutableLiveData("")
+    val onVoiceButtonClicked = MutableLiveData(false)
     val onInputButtonClicked = MutableLiveData(false)
-
-
     fun onClickInputButton() {
         onInputButtonClicked.value = true
+    }
+
+    fun onClickVoiceButton() {
+        onVoiceButtonClicked.value = true
     }
 }
