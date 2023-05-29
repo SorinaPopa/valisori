3d4cd0f515510b91239ba51f0ed15c93f9461403
valentinnistoran <valentin.nistoran@student.upt.ro>
2023-05-29 16:55:16 +0300

    The final FINAL version of the raspberrypi code

--- Raspberrypi/changer.py
+++ Raspberrypi/changer.py
@@ -1,3 +1,4 @@
+
 import pyrebase
 import RPi.GPIO as GPIO
 
@@ -6,10 +7,10 @@ GREEN_PIN = 27
 BLUE_PIN = 22
 
 config = {
-    "apiKey": "your_api_key",
-    "authDomain": "your_project_id.firebaseapp.com",
-    "databaseURL": "https://your_project_id.firebaseio.com",
-    "storageBucket": "your_project_id.appspot.com"
+    "apiKey": "AIzaSyCiyx86AFTnFwPkjNnI9CIhbG_iov6HuR8",
+    "authDomain": "valisori-72068.firebaseapp.com",
+    "databaseURL": "https://valisori-72068-default-rtdb.firebaseio.com",
+    "storageBucket": "valisori-72068.appspot.com"
 }
 
 firebase = pyrebase.initialize_app(config)
@@ -24,17 +25,19 @@ red_pwm = GPIO.PWM(RED_PIN, 100)
 green_pwm = GPIO.PWM(GREEN_PIN, 100)
 blue_pwm = GPIO.PWM(BLUE_PIN, 100)
 
-def set_color(r, g, b):
-    red_pwm.start(r * 100 / 255)
-    green_pwm.start(g * 100 / 255)
-    blue_pwm.start(b * 100 / 255)
+def set_color(r,g,b):
+    red_pwm.start(r*100/255)
+    green_pwm.start(g*100/255)
+    blue_pwm.start(b*100/255)
 
 def stream_handler(message):
     if message["event"] == "put":
         if message["path"] == "/":
-            r, g, b = message["data"]["value"]
-            print(r, g, b)
+            r,g,b = message["data"]["value"]
+            print(r,g,b)
             set_color(r, g, b)
+            
+my_stream = db.child("colors").stream(stream_handler)
 
 try:
     my_stream = db.child("colors").stream(stream_handler)
